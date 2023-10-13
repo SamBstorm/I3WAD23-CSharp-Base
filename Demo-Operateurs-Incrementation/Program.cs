ea0dc434d34c71384118fef386c25a45a3a46988
Samuel Legrain <samuel.legrain@bstorm.be>
2023-10-13 13:59:45 +0200

    Ajout Démos : Opérateurs

---
+++ Demo-Operateurs-Incrementation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo_Operateurs_Incrementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+
+            //int j = i++;
+
+            //Equivalent
+            int j = i;
+            i++;
+
+            Console.WriteLine($"i : {i} | j : {j}");
+
+            Console.WriteLine($"i + j => {i+j}");
+
+            j += i;
+            Console.WriteLine($"i : {i} | j : {j}");
+        }
+    }
+}
---
+++ Demo-Operateurs-Ternaire/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Demo_Operateurs_Ternaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez indiquer combien d'année vous reste-t-il de formation?");
+            if (int.TryParse(Console.ReadLine(), out int year))
+            {
+                //if (year > 1)
+                //{
+                //    Console.WriteLine($"Waouw! Il vous reste {year} années!");
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"Waouw! Il vous reste {year} année!");
+                //}
+
+                ////Avec ternaire et affectation
+                //string message = (year > 1) ? "s" : "";
+                //Console.WriteLine($"Waouw! Il vous reste {year} année{message}!");
+
+                //Avec ternaire dans l'exécution
+                Console.WriteLine($"Waouw! Il vous reste {year} année{((year > 1) ? "s" : "")}!");
+            }
+        }
+    }
+}
