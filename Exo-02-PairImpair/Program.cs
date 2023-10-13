588e905bf3c0d6cd29e5cbe3a9587ffd786c5556
Samuel Legrain <samuel.legrain@bstorm.be>
2023-10-13 11:08:17 +0200

    Ajout Exos : 02 et 03

---
+++ Exo-02-PairImpair/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exo_02_PairImpair
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez indiquer un nombre : ");
+            string reponse = Console.ReadLine();
+            bool isConverted = int.TryParse(reponse, out int nombre1);
+
+            if (isConverted)
+            {
+                if (nombre1 == (nombre1 / 2) * 2)
+                {
+                    Console.WriteLine($"Le nombre {nombre1} est pair.");
+                }
+                else
+                {
+                    Console.WriteLine($"Le nombre {nombre1} est impair.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"La valeur {reponse}, n'est pas un nombre. Veuillez introduire un nombre!");
+            }
+        }
+    }
+}
---
+++ Exo-03-JeuxHasard/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exo_03_JeuxHasard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Veuillez indiquer votre année de naissance (YYYY) :");
+
+            if (int.TryParse(Console.ReadLine(), out year))
+            {
+                //if(year <= DateTime.Now.Year - 18)
+                if (DateTime.Now.Year - year >= 18)
+                {
+                    Console.WriteLine("Veuillez indiquer votre nationalité SVP :");
+                    switch (Console.ReadLine())
+                    {
+                        case "Belge":
+                            Console.WriteLine("Bienvenue sur le site de la loterie nationale, une fois!");
+                            break;
+                        case "Français":
+                        case "Française":
+                            Console.WriteLine("Bienvenue sur le site de la loterie nationale! Cocorico!");
+                            break;
+                        case "Suisse":
+                            Console.WriteLine("Bienvenuuuuue sur le siteeeeuhhh de la loterie nationaleeeeeuhhh!");
+                            break;
+                        case "Canadien":
+                        case "Canadienne":
+                            Console.WriteLine("Bienvenue sur le site de la loterie nationale! Tabernacle!");
+                            break;
+                        default:
+                            Console.WriteLine("Malheureusement, vous n'êtes pas éligible...");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Vous êtes trop jeune, il vous reste {18 - (DateTime.Now.Year - year)} ans à attendre...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vous n'avez pas entrer une année valable.");
+            }
+        }
+    }
+}
