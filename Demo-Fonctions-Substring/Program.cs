8626e14ee67f4e2d85d4bfbff90678a97c716216
Samuel Legrain <samuel.legrain@bstorm.be>
2023-10-13 15:09:08 +0200

    Ajout Exo-04 + Demo-Substring

---
+++ Demo-Fonctions-Substring/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo_Fonctions_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer votre e-mail :");
+            string email = Console.ReadLine();
+
+            Console.WriteLine($"Votre email est : {email}");
+
+            string first_char = email.Substring(0, 1);
+            Console.WriteLine($"La première lettre est : {first_char}");
+
+            string three_chars_from_third = email.Substring(2, 3);
+            Console.WriteLine($"Les trois caractères qui se suivent à partir du troisième de l'email sont '{three_chars_from_third}'.");
+
+            string five_last_chars = email.Substring(email.Length - 5, 5);
+            Console.WriteLine($"Les cinq derniers caractères de votre emails sont '{five_last_chars}'");
+        }
+    }
+}
---
+++ Exo-04-Divisions/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exo_04_Divisions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez introduire un premier nombre entier : ");
+            string reponse = Console.ReadLine();
+            if(int.TryParse(reponse, out int nombre1)) {
+                Console.WriteLine("Veuillez introduire un second nombre entier : ");
+                reponse = Console.ReadLine();
+                if(int.TryParse(reponse, out int nombre2))
+                {
+                    int result_div_ent = nombre1 / nombre2;
+                    int result_modulo = nombre1 % nombre2;
+                    float result_div = (float)nombre1 / nombre2;
+                    Console.WriteLine(
+@$"Avec les nombres {nombre1} et {nombre2} nous obtenons :
+Division entière : {result_div_ent}
+Modulo : {result_modulo}
+Division avec décimal : {result_div}");
+                }
+                else { Console.WriteLine($"Le nombre '{reponse}' n'est pas un nombre entier!"); }
+            }
+            else { Console.WriteLine($"Le nombre '{reponse}' n'est pas un nombre entier!"); }
+        }
+    }
+}
