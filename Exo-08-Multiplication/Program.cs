261f9a3789488025c2d81512234deffff0085b82
Samuel Legrain <samuel.legrain@bstorm.be>
2023-10-16 14:44:05 +0200

    Ajout Exos 08 + 09

---
+++ Exo-08-Multiplication/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exo_08_Multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int table = 1; table <= 5 ; table++)
+            {
+                for (int multiple = 1; multiple <= 20; multiple++)
+                {
+                    Console.Write($"{table} * {multiple} = {table*multiple} ;\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Exo-09-Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exo_09_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f0 = 0;
+            int f1 = 1;
+            int n = 25;
+
+            Console.WriteLine($"F0 : {f0}");
+            Console.WriteLine($"F1 : {f1}");
+            for (int i = 2; i <= n; i++)
+            {
+                int f = f0 + f1;
+                Console.WriteLine($"F{i} : {f}");
+                f0 = f1;
+                f1 = f;
+            }
+        }
+    }
+}
