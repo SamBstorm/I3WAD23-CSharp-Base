583cd71c2c6f1c67d22df2478c4f679faab864fb
Samuel Legrain <samuel.legrain@bstorm.be>
2023-10-16 16:40:30 +0200

    Ajout Exos 10 + 11

---
+++ Exo-10-Factoriel/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exo_10_Factoriel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez indiquer un entier naturel à mettre sous factoriel :");
+            string? reponse = Console.ReadLine();
+            int facto;
+            bool isConverted = int.TryParse(reponse, out facto);
+            while (!isConverted)
+            {
+                Console.WriteLine($"La valeur {reponse} n'est pas un entier naturel!");
+                Console.WriteLine("Veuillez indiquer un entier naturel à mettre sous factoriel :");
+                reponse = Console.ReadLine();
+                isConverted = int.TryParse(reponse, out facto);
+            }
+            long result = 1;
+            for (int i = facto; i > 1 ; i--)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Le Factoriel de {facto} est : {result}");
+        }
+    }
+}
---
+++ Exo-11-NombresPremiers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exo_11_NombresPremiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nb;
+            bool isConverted;
+            do
+            {
+                Console.WriteLine("Veuillez indiquer le nombre de nombres premiers que vous souhaitez obtenir :");
+                string? reponse = Console.ReadLine();
+                isConverted = int.TryParse(reponse, out nb);
+            } while (!isConverted);
+
+            for (int i = 2, count = 0; count < nb; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j <= (i/2) && isPrime; j++)
+                {
+                    if(i % j == 0) isPrime = false;
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                    count++;
+                }
+            }
+
+        }
+    }
+}
