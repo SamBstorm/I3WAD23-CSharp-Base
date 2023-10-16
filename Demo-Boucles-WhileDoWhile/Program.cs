cd149ab5a0b22839b245be1ef2ef2f7763185e4b
Samuel Legrain <samuel.legrain@bstorm.be>
2023-10-16 12:24:00 +0200

    Ajout Exo + Demo Boucles

---
+++ Demo-Boucles-WhileDoWhile/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Demo_Boucles_WhileDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Demo While
+            //Console.WriteLine("Veuillez entrer un chiffre :");
+            //string reponse = Console.ReadLine();
+            //int nb;
+            //bool isConverted = int.TryParse(reponse, out nb);
+            //while (!isConverted)
+            //{
+            //    Console.WriteLine($"Erreur! Le nombre {reponse} n'est pas valide!\nVeuillez entrer un chiffre :");
+            //    reponse = Console.ReadLine();
+            //    isConverted = int.TryParse(reponse, out nb);
+            //}
+            //Console.WriteLine($"Le nombre {nb} est correct!");
+
+            #endregion
+
+            #region Demo Do While
+
+            //int nb;
+            //bool isConverted;
+            //string message = "Veuillez entrer un chiffre :";
+            //do
+            //{
+            //    Console.WriteLine(message);
+            //    string reponse = Console.ReadLine();
+            //    message = $"Erreur! Le nombre {reponse} n'est pas valide!\nVeuillez entrer un chiffre :";
+            //    isConverted = int.TryParse(reponse, out nb); 
+            //} while (!isConverted);
+            //Console.WriteLine($"Le nombre {nb} est correct!");
+
+            #endregion
+
+            #region Demo Do While (simplifié)
+
+            int nb;
+            do
+            {
+                Console.WriteLine("Veuillez entrer un chiffre :");
+            } while (!int.TryParse(Console.ReadLine(), out nb));
+            Console.WriteLine($"Le nombre {nb} est correct!");
+
+            #endregion
+        }
+    }
+}
---
+++ Exo-07-BoucleDouble/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exo_07_BoucleDouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double i = 0; i <= 20; i += 0.1)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (decimal i = 0; i <= 20M; i+= 0.1M)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i <= 200; i++)
+            {
+                Console.WriteLine(i / 10D);
+            }
+        }
+    }
+}
