ba5b767e25bbc0048e60f68da042d265497e4f32
Samuel Legrain <samuel.legrain@bstorm.be>
2023-10-17 13:48:35 +0200

    Ajout Démo ToCharArray()

---
+++ Demo-Fonctions-ToCharArray/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo_Fonctions_ToCharArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insérez un texte :");
+
+            string texte = Console.ReadLine() ?? "";
+
+            char[] texte_array = texte.ToCharArray();
+
+            foreach (char c in texte_array)
+            {
+                //Console.WriteLine((int)c - 48);
+                Console.WriteLine(int.Parse(c.ToString()));
+            }
+        }
+    }
+}
