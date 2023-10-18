78a97f235aa4092d48ff2851f6a361501dcfde3e
Samuel Legrain <samuel.legrain@bstorm.be>
2023-10-18 11:53:10 +0200

    Ajout Exo 14 Déplacement d'un Pion

---
+++ Exo-14-Pion/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Exo_14_Pion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enoncé
+             */
+            /*1. Génération du Tableau*/
+            const int TAILLE = 11;
+            bool[] tableau = new bool[TAILLE];
+
+            /*2. Définir la position du PION*/
+            int position = TAILLE / 2;
+            tableau[position] = true;
+
+            string choix;
+            do
+            {
+                /*3. Afficher le tableau + instructions*/
+                /* [     ·     ]*/
+                const char PION = '·';
+                const char VIDE = ' ';
+                Console.Clear();
+                Console.Write("[");
+                foreach (bool cell in tableau)
+                {
+                    if (cell)
+                    {
+                        Console.Write(PION);
+                    }
+                    else
+                    {
+                        Console.Write(VIDE);
+                    }
+                }
+                Console.WriteLine("]");
+                Console.WriteLine(
+    @"Faites un choix:
+'G' pour déplacer le pion sur la gauche
+'D' pour déplacer le pion sur la droite
+'Q' pour stopper l'application");
+
+                /*4. Attendre le choix de l'utilisateur*/
+                do
+                {
+                    choix = Console.ReadLine().Trim().ToUpper();
+                } while (choix != "G" && choix != "D" && choix != "Q");
+
+                /*5. a. Si choix "G":
+                        Déplacer le pion ==> retour 3.
+                       b. Si choix "D":
+                        Déplacer le pion ==> retour 3.
+                */
+                tableau[position] = false;
+                switch (choix) {
+                    case "G":
+                        if(position > 0) position--;
+                        break;
+                    case "D":
+                        if(position < tableau.Length-1) position++;
+                        break;
+                }
+                tableau[position] = true;
+
+                /* c. Si choix "Q": Tout arrêter*/ 
+            } while (choix != "Q");
+
+        }
+    }
+}
