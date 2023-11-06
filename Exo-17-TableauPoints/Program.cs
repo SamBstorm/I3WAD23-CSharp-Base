6fd89ebcf8f1355710a11f3158c21bba78af5da7
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-06 14:29:46 +0100

    Ajout Exo 17

---
+++ Exo-17-TableauPoints/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exo_17_TableauPoints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAILLE = 5;
+            Point?[,] tab = new Point?[TAILLE,TAILLE];
+
+            Point p;
+
+            for (int i = 0; i < TAILLE; i++)
+            {
+                p.x = i + 1;
+                p.y = i + 1;
+
+                tab[i, i] = p;
+            }
+
+            for (int row = 0; row < TAILLE; row++)
+            {
+                for (int col = 0; col < TAILLE; col++)
+                {
+                    if (tab[row,col] is null) Console.Write("\t");
+                    //else Console.Write($"X : {tab[row,col]?.x} - Y : {tab[row,col]?.y}");
+                    else
+                    {
+                        p = (Point)tab[row, col];
+                        Console.Write($"X : {p.x} - Y : {p.y}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
