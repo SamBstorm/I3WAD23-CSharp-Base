90a5257795eb9115a555bfa0812c1ea8dfb0df3b
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-07 16:43:49 +0100

    Ajout Démo Celsius Fahrenheit

---
+++ Demo-Fonctions-CelsiusFahrenheit/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo_Fonctions_CelsiusFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius temp1 = new Celsius();
+            temp1.temperature = 10;
+
+            Fahrenheit temp2 = temp1.ToFahrenheit();
+
+            Console.WriteLine($"{temp1.temperature} celsius vaut {temp2.temperature} fahrenheit!");
+
+            temp1 = temp2.ToCelsius();
+            Console.WriteLine($"{temp2.temperature} fahrenheit vaut {temp1.temperature} celsius!");
+        }
+    }
+}
---
+++ Demo-Fonctions-CelsiusFahrenheit/Structs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Fonctions_CelsiusFahrenheit
+{
+    public struct Celsius {
+        public double temperature;
+
+        public Fahrenheit ToFahrenheit()
+        {
+            Fahrenheit result;
+            result.temperature = temperature * 1.8 + 32;
+            return result;
+        }
+    }
+    public struct Fahrenheit { 
+        public double temperature;
+
+        public Celsius ToCelsius()
+        {
+            Celsius result;
+            result.temperature = (temperature - 32) / 1.8;
+            return result;
+        }
+    }
+}
