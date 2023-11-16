bf0d485a7536107010f730cab6eb1b861a683ef0
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-16 12:35:14 +0100

    Ajout Exo20

---
+++ Exo20-Equation/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exo_20_Equation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EqSecondDegree equation;
+            equation.A = 4;
+            equation.B = 4;
+            equation.C = 1;
+
+            double? x1, x2;
+
+            Console.WriteLine($"({equation.A}X²) + ({equation.B}X) + ({equation.C})");
+
+            if (equation.Resoudre(out x1, out x2))
+            {
+                Console.WriteLine("Il y a une solution :");
+                if (x1 == x2) Console.WriteLine($"La seule valeur de X est {x1}.");
+                else Console.WriteLine($"X peut être à la fois {x1} ou {x2}");
+            }
+            else
+            {
+                Console.WriteLine("Aucune solution...");
+            }
+        }
+    }
+}
---
+++ Exo20-Equation/Structs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_20_Equation
+{
+    public struct EqSecondDegree
+    {
+        public double A, B, C;
+
+        public bool Resoudre(out double? x1, out double? x2)
+        {
+            x1 = null;
+            x2 = null;
+
+            double delta = (B*B) - (4*A*C);
+
+            if (delta < 0) return false;
+            //if (delta == 0)
+            //{
+            //    x1 = (-B) / (2*A) ;
+            //    x2 = x1;
+            //}
+            //else
+            //{
+                x1 = (-B - Math.Sqrt(delta)) / (2 * A);
+                x2 = (-B + Math.Sqrt(delta)) / (2 * A);
+            //}
+
+            return true;
+        }
+    }
+}
