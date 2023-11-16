eb3def6752edb3ba969d6b2ae10b735a0df20696
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-16 16:45:18 +0100

    Ajout Exo 21

---
+++ Exo-21-Cartes/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_21_Cartes
+{
+    public enum Couleurs { Coeur, Carreau, Pique, Trefle }
+
+    public enum Valeurs
+    {
+        @as = 14,
+        deux = 2,
+        trois,
+        quatre,
+        cinq,
+        six,
+        sept,
+        huit,
+        neuf,
+        dix,
+        valet,
+        reine,
+        roi
+    }
+}
---
+++ Exo-21-Cartes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exo_21_Cartes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carte[] paquet = new Carte[52];
+
+            //int couleur_nb = 0;
+            //int valeur_nb = 2;
+
+            #region NE FONCTIONNE PAS (PIÈGE DU FOREACH)
+            //foreach (Carte carte in paquet)
+            //{
+            //    carte.couleur = (Couleurs)couleur_nb % 4;
+            //    carte.valeur = (Valeurs)valeur_nb % 13;
+            //} 
+            #endregion
+
+            int index = 0;
+            foreach (Couleurs couleur_carte in Enum.GetValues<Couleurs>())
+            {
+                foreach (Valeurs valeur_carte in Enum.GetValues<Valeurs>())
+                {
+                    paquet[index].couleur = couleur_carte;
+                    paquet[index].valeur = valeur_carte;
+                    index++;
+                }
+            }
+
+            foreach (Carte carte in paquet)
+            {
+                Console.WriteLine($"{carte.valeur} de {carte.couleur}");
+            }
+        }
+    }
+}
