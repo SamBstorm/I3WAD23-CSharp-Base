394aecb506e1bfa443c83a66770158f7414bc9cf
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-16 10:36:04 +0100

    Ajout Démo Fonctions Paramètres et surcharges

---
+++ Demo-Fonctions-ParametreOut/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo_Fonctions_ParametreOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathematique m = new Mathematique();
+
+            double modulo;
+            int quotient;
+            double result = m.CompleteDivisionResult(64.2, 5, out modulo, out quotient);
+
+            Console.WriteLine($"La division de 64.2 par 5 donne\nUne division entière de {quotient} avec un reste de {modulo},\nsoit en décimal {result}!");
+
+            int[] cotes = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+            Console.WriteLine($"La moyenne est de {m.Moyenne(cotes)}.");
+
+            double[] cotes2 = { 1, 2, 3.14, 4.2, 5, 6, 7, 8.5, 9.1, 10, 11 };
+            Console.WriteLine($"La moyenne est de {m.Moyenne(cotes2)}.");
+        }
+    }
+}
---
+++ Demo-Fonctions-ParametreOut/Structs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Fonctions_ParametreOut
+{
+    public struct Mathematique
+    {
+        public double CompleteDivisionResult(
+            double nb1,
+            double nb2,
+            out double moduloResult,
+            out int divisionResult)
+        {
+            moduloResult = nb1 % nb2;
+            divisionResult = (int)(nb1 / nb2);
+            return nb1 / nb2;
+        }
+
+        public double Moyenne(int[] values)
+        {
+            double[] valuesDouble = new double[values.Length];
+            for(int i=0; i< values.Length; i++)
+            {
+                valuesDouble[i] = values[i];
+            }
+            return Moyenne(valuesDouble);
+        }
+
+        public double Moyenne(double[] values)
+        {
+            double somme = 0;
+            foreach (double value in values)
+            {
+                somme += value;
+            }
+            return somme / values.Length;
+        }
+    }
+}
